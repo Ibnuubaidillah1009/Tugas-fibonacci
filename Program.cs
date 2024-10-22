3da308d47aee5c413d4df4eaee2876908befce24
Ibnuubaidillah1009 <ibnuubaidillah1009@gmail.com>
2024-10-22 07:15:24 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan jumlah bilangan Fibonacci yang ingin ditampilkan: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, next;
+
+        Console.WriteLine("Bilangan Fibonacci:");
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(a + " ");
+
+            next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
